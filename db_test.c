50e76af22f8acacf5f3b2ecdbf6610184b5e6f06
simonppg <simonppg@gmail.com>
2015-01-23 21:32:26 -0600

    correccion ya no cuenta hasta 110, sino hasta 100

--- db_test.c
+++ db_test.c
@@ -12,15 +12,16 @@ int main(int argc, char const *argv[])
 	int barra = 40;
 	int idaVuelta = 1;
 	while(1){
-		usleep(500);
+		//usleep(500);
 		if(idaVuelta == 1){//incrementa
-			(barra > 100) ? idaVuelta = 0 : (barra = barra + 10);
+			(barra > 90) ? idaVuelta = 0 : (barra = barra + 10);
 		}
 		else{//decrementa
 			(barra < 1) ? idaVuelta = 1 : (barra = barra - 10);
 		}
 		asprintf(&vector, "UPDATE variables SET barra_superior = %d WHERE 1", barra);
 		Update(vector);
+		printf("\n\n%d",barra);
 	}
 	
 
