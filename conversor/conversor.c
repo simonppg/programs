6fe6f5569180ce831b9be095d5a6f4fa13be05ef
simonppg <simonppg@gmail.com>
2015-04-28 13:36:32 -0500

    primer conversor de unidades

---
+++ conversor/conversor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int frequency; // pulsos por segundo
+	int rpm; // revoliciones por minuto
+	float ppr; // ppr pulsos por revolicion
+	float ppp; // paso por pulso
+
+	ppp = 1.8;
+	ppr = 360/ppp; 
+
+	rpm = 500;
+	frequency = (rpm/60)*ppr;
+	printf("\nfreq: %d",frequency);
+	printf("\nrpm: %d",rpm);
+
+
+	rpm = (frequency/ppr)*60;
+	printf("\nfreq: %d",frequency);
+	printf("\nrpm: %d",rpm);
+
+	printf("\n");
+	return 0;
+}
