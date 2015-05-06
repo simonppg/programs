3e8fc2c22bdd34b316e764e7b8331a5f2bb3492b
simonppg <simonppg@gmail.com>
2015-05-06 14:06:03 -0500

    fifo productor consumidor

--- listas/fifo.c
+++ listas/fifo.c
@@ -14,6 +14,9 @@ typedef struct {
 Point * new_point(int x, int y);
 int show_point(Point *p);
 int moveToPoint(Point * p);
+int move_now();
+int enqueue();
+int dequeue();
 
 Fifo fifo = {NULL, NULL};
 
@@ -25,13 +28,42 @@ int main(int argc, char const *argv[])
 	moveToPoint(new_point(4,60));
 	moveToPoint(new_point(9,10));
 
+	//Aqui debe haber dos tareas (productor, consumidor)
+	//productor: envia una serie de puntos para que el brazo se mueva y debe asegurarse de que se hayan recibido todos los puntos
+	//consumidor: mueve los motores repetando los tiempos que le toma a motor procesar la señal
+
 	printf("\n");
 	return 0;
 }
 
 int moveToPoint(Point * p)
 {
+	if(is_Fifo_Empty(fifo)) {
+		move_now(p);
+	}
+	else {
+		if(enqueue() == 0) {
+			//calcular tiempo restante
+			return remaining_time;
+		}
+		else {
+			return -1;
+		}
+	}
+}
 
+int move_now(Point * p) {
+	if(getMoving() == Waiting){
+		setMoving(Moving);
+		sleep(5); //Espera a que puede mandar otra posicion al servo
+		//TODO calcular el tiempo que debe esperar tomando en cuenta:
+		//el tiempo que le toma al servo en moverse
+		//el tiempo de planificacion de esta tarea
+		//considerar tambien el posible retraso que se genere al escribir en los archivos de Linux
+		setMoving(Waiting);
+		return 0;
+	}
+	return -1;
 }
 
 Point * new_point(int x, int y)
