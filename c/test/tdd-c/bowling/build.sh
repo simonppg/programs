e962e4a16cc1a3e7508403104647f9dee3c40e26
simonppg <simonppg@gmail.com>
2016-02-07 20:54:34 -0600

    editando build.sh

--- c/test/tdd-c/bowling/build.sh
+++ c/test/tdd-c/bowling/build.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+echo "========================================================="
+echo "TEST"
+echo "========================================================="
 gcc -std=c99 -Wall bowling_game_test.c bowling_game.c && ./a.out
+echo "========================================================="
+echo "STATIC ANALISYS"
+echo "========================================================="
+splint bowling_game_test.c
+splint bowling_game.c
+splint bowling_game.h
